0786c389780a92af9e6b796119a7a385debe1272
_N0x <tf@n0x.io>
2023-04-09 23:12:45 +0200

    Fixes to outdated lsp stuff in nvim

--- nvim/.config/nvim/lua/user/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -8,13 +8,13 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
-null_ls.setup {
-  debug = false,
-  sources = {
-    formatting.clang_format.with({
-        extra_filetypes = {"inl"},
-        extra_args = {"-style=file:~/.config/nvim/lua/user/lsp/settings/clang-format"},
+null_ls.setup({
+	debug = false,
+	sources = {
+		--formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+        formatting.clang_format.with({
+            extra_filetypes = {"inl"},
+            extra_args = {"-style=file:~/.config/nvim/lua/user/lsp/settings/clang-format"},
         }),
-    diagnostics.gccdiag,
-  },
-}
+	},
+})
