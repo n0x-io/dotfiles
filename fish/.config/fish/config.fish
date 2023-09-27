2a2a0fc40991e30728b933c6ea24a0bd6a208f9a
_N0x <tf@n0x.io>
2023-09-27 22:03:35 +0200

    fixed broken theming

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,7 @@ if [ -z "$DISPLAY" ] && [ (fgconsole) -eq 1 ];
     startx
 end
 
+# uncomment to automatically start tmux
 if status is-interactive
 and not set -q TMUX
     exec tmux
@@ -52,3 +53,8 @@ end
 function hvd
     command nmcli connection down "HP VPN"
 end
+
+function nv
+    command nvim $argv
+end
+
