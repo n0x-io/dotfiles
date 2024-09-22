335fc89219dd031d38261a01b629e7a41bec0f75
_N0x <tf@n0x.io>
2024-09-22 22:50:15 +0200

    tmux renumbering and nvim latex pluging

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -102,6 +102,9 @@ return packer.startup(function(use)
     -- dicord fuckery
     use 'andweeb/presence.nvim'
 
+    -- LaTeX
+    use 'lervag/vimtex'
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
